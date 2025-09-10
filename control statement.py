81126357719dc39c06b2426fc6a36306b44a2e81
Arathy <arathyrajeesh2@gmail.com>
2025-09-10 19:30:28 +0530

    control statement-for loop added

---
+++ control statement.py
@@ -0,0 +1,15 @@
+#For Loop 
+
+#Write a program to print numbers from 1 to 10 using a for loop.
+
+for x in range(11):
+    print(x)
+    
+    
+#Write a program to print the multiplication table of a given number.
+
+a=int(input('enter a number : '))
+
+print(f"\nMultiplication Table of {a}")
+for i in range(1, 11):
+    print(f"{a} x {i} = {a * i}")
