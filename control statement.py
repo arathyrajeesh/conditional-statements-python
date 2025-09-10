a06bc810b5b88c17578dc9660432726c96ffe86d
Arathy <arathyrajeesh2@gmail.com>
2025-09-10 19:52:33 +0530

    while loop end

--- control statement.py
+++ control statement.py
@@ -48,3 +48,62 @@ num = 10
 while num >= 1:
     print(num)
     num -= 1
+    
+    
+#Write a program to calculate the factorial of a given number using a while loop.
+
+num = int(input("Enter a number: "))
+factorial = 1
+i = 1
+
+while i <= num:
+    factorial *= i
+    i += 1
+
+print(f"The factorial of {num} is: {factorial}")
+
+
+
+
+#Write a program to print the first 10 natural numbers using a while loop.
+
+num = 1
+
+while num <= 10:
+    print(num)
+    num += 1
+    
+    
+
+
+#Write a program to reverse a given number using a while loop.
+
+num = int(input("Enter a number: "))
+reverse = 0
+
+while num > 0:
+    digit = num % 10         # Get the last digit
+    reverse = reverse * 10 + digit  # Append digit to reversed number
+    num //= 10               # Remove the last digit
+
+print("Reversed number is:", reverse)
+
+
+
+
+#Write a program to find the sum of digits of a given number using a while loop.
+
+num = int(input("Enter a number: "))
+sum_digits = 0
+
+while num > 0:
+    digit = num % 10        # find the last digit
+    sum_digits += digit     # Add digit to sum
+    num //= 10              # Remove the last digit
+
+print("The sum of digits is:", sum_digits)
+
+
+
+
+#Nested For Loop
