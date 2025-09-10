df4cfe3f702f255242581199a25c4f869b8819a6
Arathy <arathyrajeesh2@gmail.com>
2025-09-10 19:43:26 +0530

    control statement-print 10 to 1 using while

--- control statement.py
+++ control statement.py
@@ -20,3 +20,31 @@ for i in range(1, 11):
 for x in range(50):
     if x%2==0:
         print(f'{x}',end=" ")
+
+
+#Write a program to calculate the sum of numbers from 1 to 100 using a for loop.
+
+sum = 0
+
+for i in range(1, 101):
+    sum += i
+
+print("\nThe sum of numbers from 1 to 100 is:", sum)
+
+
+#Write a program to print the square of numbers from 1 to 10.
+
+
+for i in range(1, 11):
+    print(f"The square of {i} is {i*i}")
+
+
+#while loop
+
+#Write a program to print numbers from 10 down to 1 using a while loop.
+
+num = 10
+
+while num >= 1:
+    print(num)
+    num -= 1
