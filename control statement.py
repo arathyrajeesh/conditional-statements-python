c52372912b6de244242bd0767cd451dd47e6dce4
Arathy <arathyrajeesh2@gmail.com>
2025-09-10 19:34:51 +0530

    control statement-print even numbers between 1 to 50

--- control statement.py
+++ control statement.py
@@ -13,3 +13,10 @@ a=int(input('enter a number : '))
 print(f"\nMultiplication Table of {a}")
 for i in range(1, 11):
     print(f"{a} x {i} = {a * i}")
+    
+    
+#Write a program to print all even numbers from 1 to 50.
+
+for x in range(50):
+    if x%2==0:
+        print(f'{x}',end=" ")
