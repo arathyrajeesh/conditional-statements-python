a09b21693eb18393bc8ce2bae056a95f52ddbecb
Arathy <arathyrajeesh2@gmail.com>
2025-09-13 10:19:51 +0530

    text analyzer added

--- Text Analyzer .py
+++ Text Analyzer .py
@@ -1,5 +1,22 @@
-#Create a Text Analyzer that counts words, characters, and sentences in a paragraph.
+import re
 
+def text_analyzer(text):
+    characters = len(text)
 
-def text_analyzer():
-    
+    words = len(text.split())
+
+    sentences = re.split(r'[.!?]+', text)
+
+    return {
+        "Characters": characters,
+        "Words": words,
+        "Sentences": len(sentences)
+    }
+
+paragraph = "Python is a powerful programming language. It is widely used in web development, data science, and artificial intelligence"
+
+result = text_analyzer(paragraph)
+
+print("Characters:", result["Characters"])
+print("Words:", result["Words"])
+print("Sentences:", result["Sentences"])
