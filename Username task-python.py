93bd7ea8966d082f99e4cd6265a8058ec5ebdccd
Arathy <arathyrajeesh2@gmail.com>
2025-09-13 09:43:44 +0530

    usernam generator added

---
+++ Username task-python.py
@@ -0,0 +1,23 @@
+#Make a Username Generator from first and last names.
+
+
+import random
+
+def username_generator(first_name, last_name):
+    
+    first_name = first_name.lower()
+    last_name = last_name.lower()
+
+    first_part = first_name[:3] 
+
+    username = f"{first_part}{last_name}"
+
+    username += str(random.randint(10, 99))
+
+    return username
+
+first = input('Enter first name: ')
+last = input('Enter last name: ')
+
+username = username_generator(first, last)
+print(f" username for {first} {last}: {username}")
